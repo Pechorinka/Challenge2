8c7eb7c35bc0b2f9557c8858c1e5bc07ec9d4cde
Tatyana Sidoryuk <tatyanasidoryuk@Alexeys-MacBook-Air.local>
2022-08-28 22:41:04 +0300

    Comments

--- Challenge2/Controller/SecondViewController.swift
+++ Challenge2/Controller/SecondViewController.swift
@@ -5,4 +5,14 @@
 //  Created by Tatyana Sidoryuk on 28.08.2022.
 //
 
-import Foundation
+import UIKit
+
+class SecondViewController: UIViewController {
+    var networkManager = NetworkManager()
+    // @ViacheslavAT
+    // Переход на сайт фильма при нажатии на кнопку "Watch now"
+    // Загрузка описания фильма и фото и данных актеров к фильму
+    // Например networkManager.fetchActors
+    // Сделать соответствующую view
+    // Для актеров сделать collectionView + collectionViewCell
+}
