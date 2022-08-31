dc5c2a3cb74ceb68a77610437805fc47295a01e9
terentyev-va <terentyev.va@yandex.ru>
2022-08-31 22:49:39 +0300

    Create BackButton, rating, shadoweffect

--- Challenge2/Controller/SecondViewController.swift
+++ Challenge2/Controller/SecondViewController.swift
@@ -9,10 +9,21 @@ class SecondViewController: UIViewController {
         scrollView.frame = view.bounds
         return scrollView
     }()
+
+    private let shadowView: UIView = {
+       let view = UIView()
+        view.backgroundColor = .black
+        view.layer.shadowColor = UIColor.black.cgColor
+        view.layer.shadowOffset = CGSize(width: 0, height: -30)
+        view.layer.shadowRadius = 20
+        view.layer.shadowOpacity = 20
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
     
     private lazy var contentView: UIView = {
        let contentView = UIView()
-        contentView.backgroundColor = .none
+        contentView.backgroundColor = .black
         contentView.frame.size = contentsSize
         return contentView
     }()
@@ -77,10 +88,86 @@ class SecondViewController: UIViewController {
         return collectionView
     }()
     
+    private lazy var bookmarkButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setBackgroundImage(UIImage(systemName: "bookmark"), for: .normal)
+        button.tintColor = .red
+        button.addTarget(self, action: #selector(bookmarkButtonTapped), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private let genreLabel: UILabel = {
+       let label = UILabel()
+        label.text = "2020, Фантастика, Боевик, 2 ч 30 мин"
+        label.textColor = .white
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let firstStarImageView: UIImageView = {
+       let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "star.fill")
+        imageView.tintColor = .white
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private let secondStarImageView: UIImageView = {
+        let imageView = UIImageView()
+         imageView.image = UIImage(systemName: "star.fill")
+         imageView.tintColor = .white
+         imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private let thirdStarImageView: UIImageView = {
+        let imageView = UIImageView()
+         imageView.image = UIImage(systemName: "star.fill")
+         imageView.tintColor = .white
+         imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private let fourthStarImageView: UIImageView = {
+        let imageView = UIImageView()
+         imageView.image = UIImage(systemName: "star.fill")
+         imageView.tintColor = .white
+         imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private let fithStarImageView: UIImageView = {
+        let imageView = UIImageView()
+         imageView.image = UIImage(systemName: "star.fill")
+         imageView.tintColor = .white
+         imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private let stackView: UIStackView = {
+       let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.spacing = 3
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private lazy var closeButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setBackgroundImage(UIImage(systemName: "chevron.left"), for: .normal)
+        button.tintColor = .white
+        button.addTarget(self, action: #selector(closeButtonTapped), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     private var contentsSize: CGSize {
-        CGSize(width: view.frame.width, height: view.frame.height + 300)
+        CGSize(width: view.frame.width, height: view.frame.height + 400)
     }
     
+    var getStarsRating = 6
+        
     private let idCollectionView = "idCollectionView"
         
     override func viewDidLoad() {
@@ -89,18 +176,30 @@ class SecondViewController: UIViewController {
         setupViews()
         setConstraints()
         setDelegates()
+        setStars()
     }
     
     private func setupViews() {
         view.addSubview(scrollView)
         scrollView.addSubview(contentView)
         contentView.addSubview(filmImageView)
+        contentView.addSubview(shadowView)
         contentView.addSubview(nameFilmLabel)
         contentView.addSubview(descriptionLabel)
         contentView.addSubview(castLabel)
         contentView.addSubview(watchNowButton)
         contentView.addSubview(collectionView)
+        contentView.addSubview(bookmarkButton)
+        contentView.addSubview(genreLabel)
+        contentView.addSubview(closeButton)
         
+        stackView.addArrangedSubview(firstStarImageView)
+        stackView.addArrangedSubview(secondStarImageView)
+        stackView.addArrangedSubview(thirdStarImageView)
+        stackView.addArrangedSubview(fourthStarImageView)
+        stackView.addArrangedSubview(fithStarImageView)
+        contentView.addSubview(stackView)
+
         collectionView.register(DetailCollectionViewCell.self, forCellWithReuseIdentifier: idCollectionView)
     }
     
@@ -111,6 +210,45 @@ class SecondViewController: UIViewController {
     
     @objc private func watchNowButtonTapped() {
         print("Watch Now")
+    }
+    
+    @objc private func bookmarkButtonTapped() {
+        print("bookmarkButtonTapped")
+    }
+        
+    private func setStars() {
+        
+        let firstRange = 7...10
+        let secondRange = 5...7
+        let thirdRange = 4...5
+        let fourthRange = 3...4
+        let fithRange = 1...3
+        
+        if firstRange ~= getStarsRating {
+            firstStarImageView.tintColor = .systemYellow
+            secondStarImageView.tintColor = .systemYellow
+            thirdStarImageView.tintColor = .systemYellow
+            fourthStarImageView.tintColor = .systemYellow
+            fithStarImageView.tintColor = .systemYellow
+        } else if secondRange ~= getStarsRating {
+            firstStarImageView.tintColor = .systemYellow
+            secondStarImageView.tintColor = .systemYellow
+            thirdStarImageView.tintColor = .systemYellow
+            fourthStarImageView.tintColor = .systemYellow
+        } else if thirdRange ~= getStarsRating {
+            firstStarImageView.tintColor = .systemYellow
+            secondStarImageView.tintColor = .systemYellow
+            thirdStarImageView.tintColor = .systemYellow
+        } else if fourthRange ~= getStarsRating {
+            firstStarImageView.tintColor = .systemYellow
+            secondStarImageView.tintColor = .systemYellow
+        } else if fithRange ~= getStarsRating {
+            firstStarImageView.tintColor = .systemYellow
+        }
+    }
+    
+    @objc private func closeButtonTapped() {
+        dismiss(animated: true)
     }
 }
 
@@ -143,6 +281,13 @@ extension SecondViewController: UICollectionViewDelegateFlowLayout {
 extension SecondViewController {
     
     private func setConstraints() {
+
+        NSLayoutConstraint.activate([
+            shadowView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: -100),
+            shadowView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: 100),
+            shadowView.bottomAnchor.constraint(equalTo: genreLabel.topAnchor, constant: 0),
+            shadowView.heightAnchor.constraint(equalToConstant: 60)
+        ])
         
         NSLayoutConstraint.activate([
             filmImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 0),
@@ -152,25 +297,52 @@ extension SecondViewController {
         ])
         
         NSLayoutConstraint.activate([
+            bookmarkButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20),
+            bookmarkButton.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 50),
+            bookmarkButton.heightAnchor.constraint(equalToConstant: 25),
+            bookmarkButton.widthAnchor.constraint(equalToConstant: 25)
+        ])
+        
+        NSLayoutConstraint.activate([
+            closeButton.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+            closeButton.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 50),
+            closeButton.heightAnchor.constraint(equalToConstant: 30),
+            closeButton.widthAnchor.constraint(equalToConstant: 20)
+        ])
+        
+        NSLayoutConstraint.activate([
             nameFilmLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
             nameFilmLabel.bottomAnchor.constraint(equalTo: filmImageView.bottomAnchor, constant: -10)
         ])
         
         NSLayoutConstraint.activate([
+            genreLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            genreLabel.topAnchor.constraint(equalTo: filmImageView.bottomAnchor, constant: 5)
+        ])
+        
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            stackView.topAnchor.constraint(equalTo: genreLabel.bottomAnchor, constant: 10),
+            firstStarImageView.heightAnchor.constraint(equalToConstant: 20),
+            firstStarImageView.widthAnchor.constraint(equalToConstant: 20),
+            secondStarImageView.heightAnchor.constraint(equalToConstant: 20),
+            secondStarImageView.widthAnchor.constraint(equalToConstant: 20),
+            thirdStarImageView.heightAnchor.constraint(equalToConstant: 20),
+            thirdStarImageView.widthAnchor.constraint(equalToConstant: 20),
+            fourthStarImageView.heightAnchor.constraint(equalToConstant: 20),
+            fourthStarImageView.widthAnchor.constraint(equalToConstant: 20),
+            fithStarImageView.heightAnchor.constraint(equalToConstant: 20),
+            fithStarImageView.widthAnchor.constraint(equalToConstant: 20),
+        ])
+        
+        NSLayoutConstraint.activate([
             descriptionLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
             descriptionLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10),
-            descriptionLabel.topAnchor.constraint(equalTo: filmImageView.bottomAnchor, constant: 20)
+            descriptionLabel.topAnchor.constraint(equalTo: stackView.bottomAnchor, constant: 20)
         ])
-        
+                
         NSLayoutConstraint.activate([
-            watchNowButton.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
-            watchNowButton.bottomAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.bottomAnchor, constant: -10),
-            watchNowButton.heightAnchor.constraint(equalToConstant: 50),
-            watchNowButton.widthAnchor.constraint(equalToConstant: 200)
-        ])
-        
-        NSLayoutConstraint.activate([
-            castLabel.topAnchor.constraint(equalTo: filmImageView.bottomAnchor, constant: 180),
+            castLabel.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 20),
             castLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10)
         ])
         
@@ -179,6 +351,14 @@ extension SecondViewController {
             collectionView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
             collectionView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: 0),
             collectionView.topAnchor.constraint(equalTo: castLabel.bottomAnchor, constant: 5)
+        ])
+        
+        NSLayoutConstraint.activate([
+            watchNowButton.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            watchNowButton.topAnchor.constraint(equalTo: contentView.bottomAnchor, constant: 5),
+            watchNowButton.bottomAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.bottomAnchor, constant: -10),
+            watchNowButton.heightAnchor.constraint(equalToConstant: 50),
+            watchNowButton.widthAnchor.constraint(equalToConstant: 200)
         ])
     }
 }
